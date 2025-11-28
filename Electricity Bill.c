6a6bd6bc566aa4cd151bcc4210c3ba703a24e406
srikanthvarma470-maker <srikanthvarma470@gmail.com>
2025-11-28 16:00:54 +0530

    Add files via upload

---
+++ Electricity Bill.c
@@ -0,0 +1,13 @@
+//Electicity Bill
+#include<stdio.h>
+int main()
+{
+int units=50;
+int price=100;
+int amount;
+char name[30]="kamal kiran";
+printf("costumer name %s \n",name);
+amount=units*price;
+printf("amount of bill %d",amount);	
+return 0;
+}
